09a91b5b324b0161ec6c29d14d6124af5dd8177a
EmmaKibore <emmakibore@gmail.com>
2019-05-30 10:58:01 +0300

    added project

--- awards/admin.py
+++ awards/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Profile,Project,Review
 
-# Register your models here.
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('user','prof_pic','bio','contact')
+
+admin.site.register(Profile)
+admin.site.register(Project)
+admin.site.register(Review)
+
